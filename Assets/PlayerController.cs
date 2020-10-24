2688830d6c89346c439d4eefe872db8ea630486d
Ian Burnette <ian@klang-games.com>
2020-10-24 19:32:15 +0200

    setup basic character creator

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rotationSpeed;
+    public Rigidbody2D rigidbody;
+    public float accelerationSpeed;
+    
+    // Update is called once per frame
+    void Update() {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        rigidbody.AddTorque(horizontalInput * rotationSpeed * Time.deltaTime, ForceMode2D.Force);
+        
+        var verticalInput = Input.GetAxis("Vertical");
+        rigidbody.AddForce(transform.up * verticalInput * accelerationSpeed * Time.deltaTime, ForceMode2D.Force);
+    }
+}
