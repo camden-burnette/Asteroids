e6b46eb90f687b5ee0f74f1063eb1831ad563468
Ian Burnette <ian@klang-games.com>
2020-10-24 18:49:37 +0200

    rename variable

--- Assets/AsteroidSpawner.cs
+++ Assets/AsteroidSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-    public Sprite asteroidSprite;
+    public Sprite asteroidSprite2;
     // Start is called before the first frame update
     void Start()
     {
